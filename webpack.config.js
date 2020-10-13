1db5fadfc895c2f5bf9aa340066a0f6056febdcf
anabellchan <anbellchan@gmail.com>
2020-10-12 22:33:49 -0700

    Setup proxy server

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, './client/src');
+const DEST_DIR = path.join(__dirname, './client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DEST_DIR,
+    filename: 'product-bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+};
