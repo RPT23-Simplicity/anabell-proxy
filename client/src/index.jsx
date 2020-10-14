b51b2e6ebaa82d4d994d9093129f3cf1e4df0207
anabellchan <anbellchan@gmail.com>
2020-10-13 19:43:30 -0700

    Update index.html to include correct script names. Remove proxy index.jsx constructor.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,27 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    const idxPosProductId = 1;
-    const idxPosStyleId = 2;
-    const ids = window.location.pathname.split('/');
-
-    this.state = {
-      productId: ids[idxPosProductId],
-      styleId: ids[idxPosStyleId],
-      details: {},
-      style: {},
-      styles: {}
-    };
-  }
-  render() {
-    return (
-      <h1>Hello!</h1>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
