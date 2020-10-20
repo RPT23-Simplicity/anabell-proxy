be9edcb7c6a0c1a65e1a911440ed7e61de57481e
anabellchan <anbellchan@gmail.com>
2020-10-20 16:00:41 -0700

    Add API tests

--- server/index.test.js
+++ server/index.test.js
@@ -1,8 +1,66 @@
-const sum = (x, y) => {
-  return x + y;
-}
+const request = require('supertest');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+
+describe('Product Service', function () {
+  it('responds with javascript', function(done) {
+    request('http://localhost:3008')
+      .get('/product-bundle.js')
+      .expect('Content-Type', 'application/javascript; charset=UTF-8')
+      .expect(200, done);
+  });
+
+  it('responds with json', function (done) {
+    request('http://localhost:3008')
+      .get('/product/1')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
 });
 
+describe('Inventory Service', function () {
+  it('responds with javascript', function(done) {
+    request('http://localhost:3004')
+      .get('/bundle.js')
+      .expect('Content-Type', 'application/javascript; charset=UTF-8')
+      .expect(200, done);
+  });
+
+  it('responds with json', function (done) {
+    request('http://localhost:3004')
+      .get('/inventory/1/1')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+});
+
+describe('Photo Service', function () {
+  it('responds with javascript', function(done) {
+    request('http://localhost:3000')
+      .get('/bundle-photo-module.js')
+      .expect('Content-Type', 'application/javascript; charset=UTF-8')
+      .expect(200, done);
+  });
+
+  it('responds with json', function (done) {
+    request('http://localhost:3000')
+      .get('/photos/1')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+});
+
+describe('Reviews Service', function () {
+  it('responds with javascript', function(done) {
+    request('http://localhost:3003')
+      .get('/bundle.js')
+      .expect('Content-Type', 'application/javascript; charset=UTF-8')
+      .expect(200, done);
+  });
+
+  it('responds with json', function (done) {
+    request('http://localhost:3003')
+      .get('/api/reviews/1')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+});
